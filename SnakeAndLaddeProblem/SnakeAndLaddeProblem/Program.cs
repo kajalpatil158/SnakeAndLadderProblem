607f442aa07c17ec261f846defae83c6060db376
Kajal <kajalpatil158@gmail.com>
2021-03-07 01:59:23 +0530

    [Kajal Patil]Added: Snake_And_Ladder_Game_With_start

---
+++ SnakeAndLaddeProblem/SnakeAndLaddeProblem/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SnakeAndLader
+{
+    class Program
+    {
+        public const int Start = 0;
+        public const int END_POINT = 100;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Welcome To Snake And Lader Problem ");
+            string player1 = Console.ReadLine();
+            int playerPosition = Start;
+            Console.WriteLine("Current Position : " + playerPosition);
+        }
+    }
+}
