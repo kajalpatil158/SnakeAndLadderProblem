67ea7d013d4f7cb6f3caaed769147cec90fb46c5
Kajal <kajalpatil158@gmail.com>
2021-03-07 03:52:20 +0530

    [Kajal Patil]Added: Game_For_Two_Player

--- SnakeAndLaddeProblem/SnakeAndLaddeProblem/Program.cs
+++ SnakeAndLaddeProblem/SnakeAndLaddeProblem/Program.cs
@@ -2,50 +2,94 @@
 
 namespace SnakeAndLader
 {
-    class Program
-    {
-        public const int Start = 0;
-        public const int End = 100;
-        public const int No_Play = 0;
-        public const int Snake = 1;
-        public const int Ladder = 2;
+	class Program
+	{
+		public const int Start = 0;
+		public const int End = 100;
+		public const int No_Play = 0;
+		public const int Snake = 1;
+		public const int Ladder = 2;
 		public const int Initial = 0;
 		public const int Wining = 100;
-        static void Main(string[] args)
-        {
-			int CurentPosition = Initial;
-            int Moves = 0;
-            Console.WriteLine("Hello Welcome To Snake And Lader Problem ");
-            Console.WriteLine("Start Game"); while (CurentPosition != Wining)
+		static void Main(string[] args)
+		{
+			int player1 = Initial;
+			int player2 = Initial;
+			bool FirstPlayer = true;
+			Console.WriteLine("Hello Welcome To Snake And Lader Problem ");
+			Console.WriteLine("Game Start Between Two Players: ");
+			while (player1 != Wining && player2 != Wining)
 			{
-				Random value = new Random();
-				int valueOfDice = value.Next(1, 7);
-				Console.WriteLine("Player rolled : " + valueOfDice);
-				int actionTaken = value.Next(0, 3);
-				if (actionTaken == No_Play)
+				if (FirstPlayer)
 				{
-					Console.WriteLine("No action taken");
-				}
-				else if (actionTaken == Ladder)
-				{
-					CurentPosition += valueOfDice;
-					if (CurentPosition > Wining)
+
+
+					Random value = new Random();
+					int diceValue = value.Next(1, 7);
+					Console.WriteLine("First Player rolled Dice: " + diceValue);
+					int actionTaken = value.Next(0, 3);
+					if (actionTaken == No_Play)
 					{
-						CurentPosition -= valueOfDice;
+						FirstPlayer = false;
+					}
+					else if (actionTaken == Ladder)
+					{
+						player1 += diceValue;
+						if (player1 > Wining)
+						{
+							player1 -= diceValue;
+						}
+					}
+					else
+					{
+						FirstPlayer = false;
+						player1 -= diceValue;
+						if (player1 < Initial)
+						{
+							player1 = Initial;
+						}
 					}
 				}
+
 				else
 				{
-					CurentPosition -= valueOfDice;
-					if (CurentPosition < Initial)
+					Random random = new Random();
+					int diceValue = random.Next(1, 7);
+					Console.WriteLine("Second Player rolled Dice : " + diceValue);
+					Random random1 = new Random();
+
+					int actionTaken = random1.Next(0, 3);
+					if (actionTaken == No_Play)
 					{
-						CurentPosition = Initial;
+						FirstPlayer = true;
+					}
+					else if (actionTaken == Ladder)
+					{
+						player2 += diceValue;
+						if (player2 > Wining)
+						{
+							player2 -= diceValue;
+						}
+					}
+					else
+					{
+						FirstPlayer = true;
+						player2 -= diceValue;
+						if (player2 < Initial)
+						{
+							player2 = Initial;
+						}
 					}
 				}
-				Moves++;
-				Console.WriteLine("You Are At Position : " + CurentPosition);
 			}
-			Console.WriteLine("You reached exact winning position in : " + Moves + " moves");
+			if (player1 == Wining)
+			{
+				Console.WriteLine("Player 1 won the game");
+			}
+			else
+			{
+				Console.WriteLine("Player 2 won the game");
+			}
 		}
 	}
 }
