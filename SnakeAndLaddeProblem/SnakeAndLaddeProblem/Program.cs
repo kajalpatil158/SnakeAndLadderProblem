464b04fc88a45dff98f708903dca4943034f1dee
Kajal <kajalpatil158@gmail.com>
2021-03-07 02:10:50 +0530

    [Kajal Patil]Added: Roll_Dice_Randomaly

--- SnakeAndLaddeProblem/SnakeAndLaddeProblem/Program.cs
+++ SnakeAndLaddeProblem/SnakeAndLaddeProblem/Program.cs
@@ -5,13 +5,22 @@ namespace SnakeAndLader
     class Program
     {
         public const int Start = 0;
-        public const int END_POINT = 100;
+        public const int End = 100;
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Welcome To Snake And Lader Problem ");
             string player1 = Console.ReadLine();
             int playerPosition = Start;
             Console.WriteLine("Current Position : " + playerPosition);
+            int diceRoll = DiceRoll();
+            Console.WriteLine("Dice Roll : " + diceRoll);
         }
+        static int DiceRoll()
+        {
+            Random random = new Random();
+            int dice = random.Next(1, 7);
+            return dice;
+        }
+    
     }
 }
